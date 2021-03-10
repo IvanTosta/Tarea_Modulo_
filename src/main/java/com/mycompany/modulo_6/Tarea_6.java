5ae3fa3f4afda61ba20b0c5fcf3fee12a291ef41
DELL <DELL@DESKTOP-35KTPK7>
2021-03-10 00:41:50 -0600

    Tarea 6

---
+++ src/main/java/com/mycompany/modulo_6/Tarea_6.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.modulo_6;
+
+public class Tarea_6 {
+    
+    /**
+     *
+     * @param agrs
+     */
+    public static void main (string[] agrs) {
+        
+        System.out.println("Codigo de github");
+    }
+}
